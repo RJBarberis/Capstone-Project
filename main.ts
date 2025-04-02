b1517d6311de04d663816c27421104782756db3f
PioneerSwine996 <pioneerswine9@gmail.com>
2025-04-02 08:51:56 -0700

    testing

--- main.ts
+++ main.ts
@@ -1,11 +1,17 @@
 // Global variables
 let heroSprite: Sprite = null
 let projectile: Sprite = null
+let enemySprite: Sprite = null
 // Functions
 function createHeroSprite(): void {
-    heroSprite = sprites.create(img``, SpriteKind.Player)
+    heroSprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
     controller.moveSprite(heroSprite)
-    projectile = sprites.create(img``, SpriteKind.Projectile)
+    projectile = sprites.create(assets.image`myImage0`, SpriteKind.Projectile)
+    if (controller.A.isPressed()) {
+        projectile.x = heroSprite.x
+        projectile.y = heroSprite.y
+        projectile.vy = 50
+    }
 }
 // on start
 createHeroSprite()
